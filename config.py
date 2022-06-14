425a46eca683e598b4433f820de573e5344b319d
Nitishsehrawat <98394162+Nitishsehrawat@users.noreply.github.com>
2022-06-14 10:54:53 -0700

    Update config.py

--- config.py
+++ config.py
@@ -35,9 +35,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
-
-#set your Custom Caption here, Keep None for Disable Custom Caption
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>To use this bot You need to subscribe my youtube channel 👇\n</https://youtube.com/channel/UCV_vPFo1cAypZsVf_nPkB0w
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
